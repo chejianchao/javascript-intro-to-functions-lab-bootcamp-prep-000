0ac15fd8e5e4076139027e574b1b4b938906e1e9
chejianchao <820992376@qq.com>
2018-06-19 01:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function shout(str){
   return str.toUpperCase()
 }
+
+function whisper(str){
+  return str.toLowerCase()
+}
+
+function logShout(str){
+  console.log(shout(str))
+}
+
+function logWhisper(str){
+  console.log(whisper(str))
+}
+
